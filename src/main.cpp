babaebf46f38719d69fa25064eaf5a5938f725de
Alexander Andorf <aledorf@gmx.de>
2022-04-30 16:19:53 +0200

    Mein erstes Zeug

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
 #include <Arduino.h>
-#include <RoboterTask.h>
-
-
-RoboterTask RoboterLoop;
-RoboterTask RobotTask();
-
 
 void setup() {
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }

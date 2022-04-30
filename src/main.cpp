3e7448a2d77bb9b1b9708826bb8d341af492296e
Zäch <60199908+scxryshop@users.noreply.github.com>
2022-04-30 15:44:51 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <RoboterTask.h>
+
+
+RoboterTask RoboterLoop;
+RoboterTask RobotTask();
+
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
